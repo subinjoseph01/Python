50e599455e4d1f6820f6aa82d16e24cd75b9bc68
subin3439 <josephsubinn@gmail.com>
2025-07-30 16:06:37 +0530

    data type

---
+++ Learningpython04.py
@@ -0,0 +1,15 @@
+
+
+# how to find which data type are we using 
+
+
+a = 55
+b = "rom"
+
+print(type(a))
+print(type(b))
+
+
+# so here "type" indicates which datatype are we using 
+
+# in the result it shows : (int and  string ) 
