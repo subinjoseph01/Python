e11ab0e55984f629f0ec8ecbfce57192426f3a98
subin3439 <josephsubinn@gmail.com>
2025-07-30 15:50:35 +0530

    hello

---
+++ Learningpython02.py
@@ -0,0 +1,16 @@
+name = "subun"
+
+age = 123
+
+place= " loca"
+
+
+print(f"my age is{age} my name is {name} my place is {place}")
+
+
+
+# f= format . it is one of  easy way to print 
+
+# 0r  
+
+# print ("my name is ",name)
