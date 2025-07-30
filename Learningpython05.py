928f3c51e028ba3d83da094a38ed55d8f7686c70
subin3439 <josephsubinn@gmail.com>
2025-07-30 17:12:36 +0530

    userinput

---
+++ Learningpython05.py
@@ -0,0 +1,23 @@
+a = input("Enter a value:")
+b = input("Enter a value:")
+
+# or
+
+
+#  if its coming with number to get the user input 
+ 
+#  printing be like:
+ 
+#  a = int (input("Enter a value:"))
+# b = int input("Enter a value:"))
+ 
+
+# total = a + b
+
+print(a, b)
+
+
+
+# Here  to get user input in  nuber formation or lets say any number
+
+# To get the number executed add "int "  before the input
