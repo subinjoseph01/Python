c730f50ff57a9ec82cfc2582712722f27928f8f1
subin3439 <josephsubinn@gmail.com>
2025-07-30 15:34:57 +0530

    py-explanation

---
+++ Learning Python01.py
@@ -0,0 +1,34 @@
+# a =10
+# b =12
+# c=0
+
+# temp = a
+
+# a = b
+# b=c
+# c=temp
+
+# print(" Answer", a)
+# print(f" Answer {b}", b)
+# print(" Answer", c)
+
+
+# list # []
+
+# tupil ()
+
+# DICTIONARY = {23:34}
+
+# print(DICTIONARY[23])
+
+int =[34,35,45,43]
+print(f"my list is {int}")
+
+dict_name = {"Name":"Subin","Age":25}
+print(f"my name is {dict_name['Name']}")
+a= dict_name["Name"]
+
+print(a)
+
+ 
+
