be46b1dc83d28ba3bd7b082e37188a25bebc0b22
subin3439 <josephsubinn@gmail.com>
2025-08-11 11:22:30 +0530

    set

--- Learningpython10.py
+++ Learningpython10.py
@@ -13,13 +13,14 @@ else:
  print("you are too young for this trip ")
  
  
- 
-#  Here we have done the problem 
-#  School Trip Permission
-# Ask the user to enter age and check whether they have parental permission (yes/no).
-# •⁠  ⁠If the age is 10 or more, then:
-# o	If the user has parental permission, print: "You can go on the school trip."
-# o	Otherwise, print: "You need parental permission to go on the school trip."
-# •⁠  ⁠If the age is less than 10, print: "You are too young for the school trip."
+"""
+ Here we have done the problem 
+ School Trip Permission
+Ask the user to enter age and check whether they have parental permission (yes/no).
+•⁠  ⁠If the age is 10 or more, then:
+o	If the user has parental permission, print: "You can go on the school trip."
+o	Otherwise, print: "You need parental permission to go on the school trip."
+•⁠  ⁠If the age is less than 10, print: "You are too young for the school trip."
 
-# points noted : intentation is imporatant for each and every line of code 
+points noted : intentation is imporatant for each and every line of code 
+"""
---
+++ Learningpython13.py
@@ -0,0 +1,28 @@
+
+# intersection (it prints what is common in both and avoid the rest)
+# a = {"sean","sam","andy","cali","den"}
+# b = {"sean","tori","tim","cali","den","timber","sara"}
+
+# print(a&b)
+
+# union (it print what is common and also what is also remaining , but it consider duplicate as 1 )
+# a = {"sean","sam","andy","cali","den"}
+# b = {"sean","tori","tim","cali","den","timber","sara"}
+
+# print(a|b)
+
+# set difference (it doesn't print what exist in b , but print what only not common in b too )
+# a = {"sean","sam","andy","cali","den"}
+# b = {"sean","tori","tim","cali","den","timber","sara"}
+
+# print(a-b)  
+
+# symmetric difference ( b compares with a and avoid what is common and print what does not exist in a )
+
+# a = {"sean","sam","andy","cali","den"}
+# b = {"sean","tori","tim","cali","den","timber","sara"}
+
+# print(a^b)  
+
+
+
