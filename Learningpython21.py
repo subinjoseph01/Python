d6b849e1667eaea3ca38bd37965f3bf1d4a1a1e6
subin3439 <josephsubinn@gmail.com>
2025-08-12 13:07:11 +0530

    continue

---
+++ Learningpython21.py
@@ -0,0 +1,12 @@
+for mail in "something@gmail.com":
+    if mail == "@":
+        continue
+    elif mail == ".":
+        
+        continue
+    
+    else:
+        print(mail)
+    
+    
+    # here we have revmoved @ and dot(.) using continue concept
