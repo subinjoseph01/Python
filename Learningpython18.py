48b8509ec84a64c2a2042a0182cfc866ce1bbf30
subin3439 <josephsubinn@gmail.com>
2025-08-12 11:18:12 +0530

    range

---
+++ Learningpython18.py
@@ -0,0 +1,24 @@
+
+#if you want to print 1 to 10 or to any numbers we use the function called RANGE. (start to stop) & (start stop step)
+# if you want to print odd or even number or multiply by 2 , 3 ,5  use step method 
+num = range(2 ,12)
+print(set(num))
+
+Ans = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11} 
+
+# Here we print step method 
+# Here we used step method printed using  3 
+
+num = range(0 , 50 ,3)
+print(list(num))
+
+Ans = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48]
+
+
+
+#here we print 100 to 0 reverse format  (by -5 number se we get revers in count)
+
+num = range(50 , 0 ,-5)
+print(list(num))
+
+Ans = [50, 45, 40, 35, 30, 25, 20, 15, 10, 5]
